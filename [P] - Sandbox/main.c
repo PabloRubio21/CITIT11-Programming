58fb99fdbbaacacaab08fbadc3c5ef94c31747d9
Jorge Benjumea Burillo <me@intron014.com>
2022-11-05 21:55:00 +0100

    [TP] - Practica 7 (A medio hacer)
    Se me complicó :)

--- [P] - Sandbox/main.c
+++ [P] - Sandbox/main.c
@@ -1,19 +1,31 @@
 #include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
 
-#define F 10
-#define C 15
+int main(){
+    int master[3][3];
+    int array[9];
+    int f, c, pos=0, aux2;
 
-int matrix(int m[F][C]){
-   int i, j;
-   for(i=0, j=0;i<F&&m[i][j]>=0;) {
-       for (j = 0; j < C && m[i][j] >= 0; j++);
-       if(j==C) {
-           i++;
-           j=0;
-       }
+    printf("Fill array");
 
-   }
-    return (i==F);
+    for(c=0;c<=2;c++, pos++){
+        for(f=2; f>0;f--, pos++){
+            array[pos]=master[f][c];
+        }
+        array[pos]=master[f][c];
+    }
+    for(c=0;c<=1;c++, pos++){
+        for(f=2; f>0;f--, pos++){
+            array[pos]=master[f][c];
+        }
+        array[pos]=master[f][c];
+    }
+    for(c=0;c<=0;c++, pos++){
+        for(f=2; f>0;f--, pos++){
+            array[pos]=master[f][c];
+        }
+        array[pos]=master[f][c];
+    }
+
+    for(pos=0;pos<9;pos++)
+        printf("%i", array[pos]);
 }
