bb19c588dec5efeb5823319615ed56538962c341
PabloRubio21 <pablo.rubio.ru@gmail.com>
2022-11-06 13:58:37 +0100

    Update Practica5.c

--- Taller de programacion/Practica5.c
+++ Taller de programacion/Practica5.c
@@ -1,73 +1,67 @@
 #include <stdio.h>
 
-//Prototipos
+int input_power(int*, int*);
+void calculate_power(int, int);
+void print_binary(int);
 
-int scan(int*, int*);
-void  potencia(int ,int );
-void print_binario(int );
-void pulsar();
-
-//Main
 int main() {
-    int base, exp;
-    char ans;
+    int base, exponent;
+    char answer;
     do {
-        system("cls");
         do {
             system("cls");
-            scan(&base,&exp);
-            printf("Datos Introducidos:\nBase: %i\nExponente: %i\nSon estos datos correctos? (y/n)\n", base, exp);
+            input_power(&base, &exponent);
+            printf("Datos Introducidos:\nBase: %i\nExponente: %i\nSon estos datos correctos? (y/n): ", base, exponent);
             fflush(stdin);
-            scanf("%c", &ans);
-        } while (ans == 'N' || ans == 'n');
-        potencia(base, exp);
-        printf("Desea repetir?(y/n)");
+            scanf("%c", &answer);
+        } while (answer == 'N' || answer == 'n');
+
+        calculate_power(base, exponent);
+
+        printf("Desea repetir? (y/n):");
         fflush(stdin);
-        scanf("%c", &ans);
-    }while(ans=='y'||ans=='Y'||ans=='\n');
+        scanf("%c", &answer);
+    } while(answer == 'y' || answer == 'Y' || answer == '\n');
 }
 
-//Funcion
-
-int scan(int*b, int*e){
+int input_power(int *base, int *exponent){
     do {
         printf("Introduzca la base [2-10]: ");
         fflush(stdin);
-        scanf("%i", b);
-    } while (*b < 2 || *b > 10);
+        scanf("%i", base);
+    } while (*base < 2 || *base > 10);
+
     do {
         printf("Introduzca el exponente [0-10]: ");
         fflush(stdin);
-        scanf("%i", e);
-    } while (*e < 0 || *e > 10);
+        scanf("%i", exponent);
+    } while (*exponent < 0 || *exponent > 10);
 }
 
-void potencia (int b, int e){
-    int pos, r;
+void calculate_power(int base, int exponent){
+    int position = 0, decimal;
+
     system("cls");
-    if (e == 0) {
-        printf("Base: %i\nExponente: %i\nDecimal: 1\n---\n", b, pos);
+    if (exponent == 0) {
+        printf("%i^%i=1\n---\n", base, position);
         printf("Bit 0: 1\n");
         printf("---\n");
     } else {
-        for (pos = 1, r = 1; pos <= e; pos++) {
+        decimal = 1;
+        for (position = 1; position <= exponent; position++) {
             system("cls");
-            printf("Base: %i\nExponente: %i\nDecimal: %i\n---\n", b, pos, r *= b);
-            print_binario(r);
-            pulsar();
+            printf("%i^%i=%i\n---\n", base, position, decimal *= base);
+            print_binary(decimal);
+            printf("---\nPULSE ENTER PARA CONTINUAR\n");
+            getchar();
         }
     }
 }
 
-void print_binario (int r){
-    int posbin=0;
-    float bin;
-    bin=r;
-    while (bin >= 1)
-        printf("Bit %d: %d \n", posbin++, ((bin=bin / 2) - (int) bin) >= 0.5 ? 1 : 0);
-}
-
-void pulsar(){
-    printf("---\nPULSE CUALQUIER TECLA PARA CONTINUAR\n");
-    getchar();
+void print_binary (int decimal){
+    int position_binary = 0;
+    float binary;
+    binary = decimal;
+    while (binary >= 1)
+        printf("Bit %d: %d \n", position_binary++, ((binary = binary / 2) - (int) binary) >= 0.5 ? 1 : 0);
 }
