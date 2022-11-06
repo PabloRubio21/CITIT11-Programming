3ddbb3073adf2756c8991e0d2abbf7083b31db28
PabloRubio21 <pablo.rubio.ru@gmail.com>
2022-11-06 12:56:27 +0100

    Update Practica3.c

--- Taller de programacion/Practica3.c
+++ Taller de programacion/Practica3.c
@@ -2,32 +2,39 @@
 
 int main()
 {
-    float n, n2;
-    int pos;
-    char ans;
+    float input, integer, decimals;
+    int position;
+    char answer;
+
     do{
-        pos=0;
         printf("Introduzca un numero: ");
         fflush(stdin);
-        scanf("%f", &n);
-        n2=n-(int)n;
-        printf("Ha introducido el numero %f\n", n);
-        if(n>1) {
-            printf("----Parte Entera (%f)----\n", n - n2);
-            while (n >= 1)
-                printf("Bit %d: %d \n", pos++, ((n = n / 2) - (int) n) >= 0.5 ? 1 : 0);
+        scanf("%f", &input);
+        printf("Ha introducido el numero %f\n", input);
+
+        integer = input;
+        decimals = integer - (int) integer;
+
+        if (integer > 1) {
+            printf("--- Parte entera (%f) ---\n", integer - decimals);
+            position = 0;
+            while (integer >= 1)
+                printf("Bit %d: %d \n", position++, (integer = integer / 2) - (int) integer >= 0.5 ? 1 : 0);
         }
-        if(n2>0) {
-            printf("----Parte Decimal (%f)----\n", n2);
-            pos = 0;
+
+        if (decimals > 0) {
+            printf("--- Parte decimal (%f) ---\n", decimals);
+            position = 0;
             do {
-                printf("Bit %i: %i\n", pos++, (n2 *= 2) >= 1 ? 1 : 0);
-                n2 = n2 - (int) n2;
-            } while (pos <= 23);
+                printf("Bit %i: %i\n", position++, (decimals *= 2) >= 1 ? 1 : 0);
+                decimals = decimals - (int) decimals;
+            } while (position <= 23);
         }
+
         printf("Desea repetir la experiencia inolvidable que es convertir un numero a binario? (y/n)");
         fflush(stdin);
-        scanf("%c", &ans);
-    } while(ans=='y'||ans=='Y'||ans=='\n');
-    printf("Gracias por utilizar el sistema conversion a binario! \nMade by Jorge B. \n\n\n");
+        scanf("%c", &answer);
+    } while (answer == 'y' || answer == 'Y' || answer == '\n');
+
+    printf("Gracias por utilizar el sistema de conversion a binario!");
 }
