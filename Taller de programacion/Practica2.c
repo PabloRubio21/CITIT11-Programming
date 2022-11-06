42b73ff7d1cad3b615c3e020908dbea26c6d48d3
PabloRubio21 <pablo.rubio.ru@gmail.com>
2022-11-06 12:33:59 +0100

    Update Practica2.c

--- Taller de programacion/Practica2.c
+++ Taller de programacion/Practica2.c
@@ -2,22 +2,27 @@
 
 int main()
 {
-    float n;
-    int pos;
-    char ans;
-    do{
-        pos=0;
-        do{
+    float input;
+    int position;
+    char answer;
+
+    do {
+        do {
             printf("Introduzca un numero entre el 0 y el 256: ");
             fflush(stdin);
-            scanf("%f", &n);
-        } while(n<=0||n>256);
-        printf("Ha introducido el numero %f\n", n);
-        while(n>=1)
-            printf("Bit %d: %d \n", pos++, ((n=n/2)-(int)n>=0.5)?1:0);
+            scanf("%f", &input);
+        } while (input<=0 || input>256);
+        printf("Ha introducido el numero %f\n", input);
+
+        position = 0;
+        while (input >= 1)
+            printf("Bit %d: %d \n", position++, ((input = input / 2) - (int) input >= 0.5)? 1 : 0);
+
         printf("Desea repetir la experiencia inolvidable que es convertir un numero a binario? (y/n)");
         fflush(stdin);
-        scanf("%c", &ans);
-    } while(ans=='y'||ans=='Y'||ans=='\n');
-    printf("Gracias por utilizar el sistema conversion a binario! \nMade by Jorge B. \n\n\n");
+        scanf("%c", &answer);
+    } while (answer == 'y' || answer == 'Y' || answer == '\n');
+
+    printf("Gracias por utilizar el sistema de conversion a binario!");
 }
+
